24519512819b2b422886dcceb6e3607021b0f942
EC2 Default User <ec2-user@ip-172-31-35-194.ap-northeast-1.compute.internal>
2024-02-06 13:07:09 +0000

    暫定的なlogout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get    "/signup",  to: "users#new"
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
-  delete "/logout",  to: "sessions#destroy",　as: 'logout'
+  delete "/logout",  to: "sessions#destroy";
   resources :users
 end
